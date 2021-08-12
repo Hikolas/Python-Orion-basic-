37607d895cbf00ab79fb2c7c1bbd6f281d2fefbb
Kolya Voznesenskiy <kcorejz@gmail.com>
2021-08-12 15:22:01 +0300

    Data cleaning and work load number 15

--- homeworks/arguments_serialization/userlist.py
+++ homeworks/arguments_serialization/userlist.py
@@ -1,6 +1,15 @@
 import argparse
 import json
 
+
+class RepetOfInfo(Exception):
+    pass
+
+def operations():
+    for data in users_data:
+        if args.username == data.get("username") or args.email == data.get("email"):
+            raise RepetOfInfo
+
 parser = argparse.ArgumentParser()
 
 parser.add_argument("--username", help="Your user username")
@@ -11,14 +20,19 @@ user_dict = {}
 if args.username:
     user_dict['username'] = args.username
 
-if args.first_name:
+if args.email:
     user_dict['email'] = args.email
 
 user_file = open('users.json', 'r')
 users_data = json.loads(user_file.readline())
 user_file.close()
 
-users_data.append(user_dict)
+try:
+    operations()
+except RepetOfInfo:
+    print("Error: This mail or username is in use")
+else:
+    users_data.append(user_dict)
 
 user_file = open('users.json', 'w')
 user_file.write(json.dumps(users_data))
